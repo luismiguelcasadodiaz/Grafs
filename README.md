e6c5653cf3cd72d96f7806044e082c4de13ec24d
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2021-12-29 15:57:32 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este repositorio contiene los ejercicios de grafos para preparar el examen de infromatica de Luis en la ETSEIB de enero 2022
---
+++ test_one.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 15:51:22 2021
+
+@author: LuisMiguel
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
